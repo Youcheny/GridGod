d30003fc14d96aae1f9d8676473e7104549349a8
Youcheng Ye <youcheny@usc.edu>
2020-02-07 15:14:22 -0800

    downgrade to Unity version 2018.4.16f1

---
+++ Assets/GridManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+	private int rows = 8;
+	private int cols = 6;
+	private float tileSize = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid() {
+    	GameObject referenceTile = (GameObject)Instantiate(Resources.Load("Tile"));
+    	float centerOffsetX = -cols * tileSize / 2; // center
+    	float centerOffsetY = rows * tileSize / 2;
+    	for (int i = 0; i < rows; ++i) {
+    		for (int j = 0; j < cols; ++j) {
+    			GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+    			float posX = j * tileSize + centerOffsetX;
+    			float posY = i * -tileSize + centerOffsetY;
+    			tile.transform.position = new Vector2(posX, posY);
+    		}
+    	}
+    	Destroy(referenceTile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
