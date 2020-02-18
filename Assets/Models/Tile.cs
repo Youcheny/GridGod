ae701cb7808fb84278978dc8d19ccd26c9028f2f
Haozhe Chen <haozhech@usc.edu>
2020-02-18 10:33:37 -0800

    add Tile class test

---
+++ Assets/Models/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Models
+{
+    public class Tile
+    {
+        public GameObject tile;
+        public string type;
+
+        public Tile()
+        {
+
+        }
+    }
+}
+
