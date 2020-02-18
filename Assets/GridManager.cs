6107e37862e4f2cdd38c4d576527d4fd7eb56aa9
zhixul <zhixul@usc.edu>
2020-02-18 14:21:16 -0800

    grid

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -37,20 +37,64 @@ public class GridManager : MonoBehaviour
             type = "ObstacleTile"
         };
 
-        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("ObstacleTile"));
+        Tile normal = new Tile()
+        {
+            tile = (GameObject)Instantiate(Resources.Load("Tile")),
+            type = "Tile"
+        };
+
+        Tile start = new Tile()
+        {
+            tile = (GameObject)Instantiate(Resources.Load("TrapTile")),
+            type = "TrapTile"
+        };
+
+        Tile end = new Tile()
+        {
+            tile = (GameObject)Instantiate(Resources.Load("TrapTileActivated")),
+            type = "TrapTileActivated"
+        };
+
+        
+
         float centerOffsetX = -cols * tileSize / 2; // center
         float centerOffsetY = rows * tileSize / 2;
         for (int i = 0; i < rows; ++i)
         {
             for (int j = 0; j < cols; ++j)
             {
-                GameObject tile = (GameObject)Instantiate(obstacle.tile, transform);
+                GameObject tile;
+
+                if (i == 0 && j == 0)
+                {
+                    tile = (GameObject)Instantiate(start.tile, transform);
+                    
+                }
+                else if(i == rows - 1 && j == cols - 1)
+                {
+                    tile = (GameObject)Instantiate(end.tile, transform);
+                }
+                else if(i == 3 && j == 3)
+                {
+                    tile = (GameObject)Instantiate(obstacle.tile, transform);
+                }
+                else
+                {
+                    tile = (GameObject)Instantiate(normal.tile, transform);
+                }
+
                 float posX = j * tileSize + centerOffsetX;
                 float posY = i * -tileSize + centerOffsetY;
                 tile.transform.position = new Vector2(posX, posY);
+
             }
         }
-        Destroy(referenceTile);
+
+
+        Destroy(obstacle.tile);
+        Destroy(normal.tile);
+        Destroy(start.tile);
+        Destroy(end.tile);
     }
 
     // Update is called once per frame
