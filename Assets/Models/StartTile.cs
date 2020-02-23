ac771c282d429280f5f73ee9e9f047bc5a2ec66b
Haozhe Chen <haozhech@usc.edu>
2020-02-22 18:31:49 -0800

    refactor tiles

---
+++ Assets/Models/StartTile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class StartTile : Tile
+    {
+        public bool IsPassable()
+        {
+            return true;
+        }
+    }
+}
---
+++ Assets/Models/TrapTile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TrapTile : Tile
+    {
+        public bool Vulnerable = false;
+
+        public bool IsPassable()
+        {
+            if(Vulnerable)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
