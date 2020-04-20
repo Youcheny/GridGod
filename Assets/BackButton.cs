1841e22472d4ad335572f7cfd6c7bb60405d6010
Hanqi Ni <hni@usc.edu>
2020-04-20 00:16:39 -0700

    add shop scene

---
+++ Assets/BackButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour
+{
+    public Button playBtn;
+
+    void Start()
+    {
+        Button btn = playBtn.GetComponent<Button>();
+        btn.onClick.AddListener(Back);
+    }
+
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("SampleScene"));
+    }
+}
---
+++ Assets/ShopMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ShopMenu : MonoBehaviour
+{
+	public GameObject levelButtonContainer;
+
+    public void Start()
+    {
+        levelButtonContainer = GameObject.Find("Main Camera/Canvas/UnlockMenu");
+        Button[] buttons = levelButtonContainer.GetComponentsInChildren<Button>();
+        print("len: " + buttons.Length);
+        foreach (Button button in buttons)
+        {
+            string text = button.GetComponentInChildren<TextMeshProUGUI>().text;
+            button.onClick.AddListener(delegate { UnlockLevel(int.Parse(text)); });
+        }
+    }
+
+    private void UnlockLevel(int level)
+    {
+    	if (CoinManager.numOfCoins >= 5)
+    	{
+    		// TODO: actually unlock levels
+
+    		// deduct coins
+    		CoinManager.numOfCoins-=5;
+    	}
+        else
+        {
+            print ("coins are not enough");
+        }
+        
+        
+    }
+}
